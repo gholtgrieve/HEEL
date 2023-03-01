4b982178f61ec62b8e5bedc6bd137603ea6bc5ff
gholtgrieve <gholt@uw.edu>
2023-02-28 16:39:30 -0800

    Working the bugs out...

--- R/EA.plot.standards.R
+++ R/EA.plot.standards.R
@@ -60,23 +60,23 @@ EA.plot.standards <- function(results){
       geom_smooth(method=lm, formula = y ~ x, se = FALSE) +
       theme_minimal() +
       theme(legend.position = "none") +
-      ggpubr::stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+      ggpubr::stat_cor(aes(label = paste(after_stat(rr.label), after_stat(p.label), sep = "~`,`~")),
                label.y.npc = "center") +
       ggpubr::stat_regline_equation()
     p2 <- ggplot(standard.CN[standard.CN$group=="GA1"|standard.CN$group=="GA2",], aes(x = mass.C.mg, y = Area.44)) +
       geom_point() +
       labs(x = "mass (mg)", title = "Area Response vs. Input C") +
       geom_smooth(method=lm, formula = y ~ x, se = FALSE) +
-      ggpubr::stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+      ggpubr::stat_cor(aes(label = paste(after_stat(rr.label), after_stat(p.label), sep = "~`,`~")),
                label.y.npc = "center") +
       theme_minimal() +
       ggpubr::stat_regline_equation()
-    p21 <- gridExtra::grid.arrange(p2, p1, ncol = 2)
+    p21 <- ggsave("p21.png",gridExtra::grid.arrange(p2, p1, ncol = 2))
 
     #Save linear models for mass calculations later
     temp2 <- standard.CN[standard.CN$group == "GA2" | standard.CN$group == "GA1",]
-    N.mass.vs.Area28.lm.coeff <- coefficients(lm(temp2$mass.N.mg ~ temp$Area.28))
-    C.mass.vs.Area44.lm.coeff <- coefficients(lm(temp2$mass.C.mg ~ temp$Area.44))
+    N.mass.vs.Area28.lm.coeff <- summary(lm(temp2$mass.N.mg ~ temp$Area.28))$coefficients[2,c(1,4)]
+    C.mass.vs.Area44.lm.coeff <- summary(lm(temp2$mass.C.mg ~ temp$Area.44))$coefficients[2,c(1,4)]
 
 
     # PLOT 2 -----------------------------
@@ -85,7 +85,7 @@ EA.plot.standards <- function(results){
       p5 <- ggplot(temp, aes(x = Area.28, y = d15N, group = group, color = group)) +
         geom_point() +
         labs(y = "d15N", title = "d15N vs m/z 28") +
-        ggpubr::stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+      ggpubr::stat_cor(aes(label = paste(after_stat(rr.label), after_stat(p.label), sep = "~`,`~")),
                 label.y.npc = "center") +
         ggpubr::stat_regline_equation(label.y.npc = "top") +
         geom_smooth(method = lm, formula = y ~ x, se = FALSE) +
@@ -94,7 +94,7 @@ EA.plot.standards <- function(results){
       p6 <-  ggplot(temp, aes(x = Area.44, y = d13C, group = group, color = group)) +
         geom_point() +
         labs(y = "d13C", title = "d13C vs m/z 44 ") +
-        ggpubr::stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+        ggpubr::stat_cor(aes(label = paste(after_stat(rr.label), after_stat(p.label), sep = "~`,`~")),
                 label.y.npc = "center") +
         ggpubr::stat_regline_equation(label.y.npc = "top") +
         theme_minimal() +
@@ -102,12 +102,12 @@ EA.plot.standards <- function(results){
       p65 <- gridExtra::grid.arrange(p6, p5, ncol = 2)
 
       #Save linear models for corrections later
-      d15N.vs.Area28.lm.coeff <- coefficients(lm(temp$d15N ~ temp$Area.28))
-      d13C.vs.Area44.lm.coeff <- coefficients(lm(temp$d13C ~ temp$Area.44))
+      d15N.vs.Area28.lm.coeff <- summary(lm(temp$d15N ~ temp$Area.28))$coefficients[2,c(1,4)]
+      d13C.vs.Area44.lm.coeff <- summary(lm(temp$d13C ~ temp$Area.44))$coefficients[2,c(1,4)]
 
       temp <- data.frame(Model=c("d13C.vs.Area44", "d15N.vs.Area28", "MassC.vs.Area44", "MassN.vs.Area28"),
                 rbind(d13C.vs.Area44.lm.coeff, d15N.vs.Area28.lm.coeff, C.mass.vs.Area44.lm.coeff, N.mass.vs.Area28.lm.coeff))
-      colnames(temp) <- c("Model","Intercept", "Slope")
+      colnames(temp) <- c("Model","Slope", "Pr(>|t|)")
       standard.coefficients <- tibble(temp)
 
       return(list(standard.plots=list(p21,p65), standard.coefficients))
